361a332f1e979eed201c2d781150a6e37a93f3b9
kaiguy920 <smithkai920@gmail.com>
2022-08-22 16:22:42 -0400

    modifies random color function

--- DOM2/Event Listeners/test1.js
+++ DOM2/Event Listeners/test1.js
@@ -8,6 +8,8 @@ circle.addEventListener('click', changeColor)
 
 function changeColor() {
     let randomColor = Math.floor(Math.random() * 16777216).toString(16)
+    // making sure our hex number always has 6 digits, we can add/pad "0"s to the beginning of our string to ensure that the total string length is always 6
+    randomColor = randomColor.padStart(6, "0")
     console.log(randomColor);
     circle.style.backgroundColor = `#${randomColor}`
 }
