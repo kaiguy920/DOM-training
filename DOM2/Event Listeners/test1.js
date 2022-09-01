6540e81a54adbe12e47c6eeee93a639f94c28232
kaiguy920 <smithkai920@gmail.com>
2022-09-01 15:27:58 -0400

    adds form html example

--- DOM2/Event Listeners/test1.js
+++ DOM2/Event Listeners/test1.js
@@ -7,6 +7,7 @@ let circle = document.getElementById('circle')
 circle.addEventListener('click', changeColor)
 
 function changeColor() {
+    // use a base of 16 as the hexadecimal numerical systerm has 16 different types of digits
     let randomColor = Math.floor(Math.random() * 16777216).toString(16)
     // making sure our hex number always has 6 digits, we can add/pad "0"s to the beginning of our string to ensure that the total string length is always 6
     randomColor = randomColor.padStart(6, "0")
